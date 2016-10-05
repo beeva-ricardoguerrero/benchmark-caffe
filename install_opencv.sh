d51115b67531186ea1e3b9365388519282dad2ac
Ricardo <ricardo.guerrero@beeva.com>
2016-10-05 15:01:49 +0200

    Fixed path issue

--- install_opencv.sh
+++ install_opencv.sh
@@ -35,7 +35,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
 	-D INSTALL_C_EXAMPLES=OFF \
 	-D INSTALL_PYTHON_EXAMPLES=ON \
-	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+	-D OPENCV_EXTRA_MODULES_PATH=~/repos/opencv_contrib/modules \
 	-D BUILD_EXAMPLES=ON ..
 
 # This line
@@ -51,3 +51,4 @@ sudo ldconfig
 # For test
 #python
 #import cv2
+
